57d0db48cf5140c0e3f1b7765341f9fd02da453b
ampp33 <ampp33@gmail.com>
2017-03-01 20:45:24 -0500

    next round of construction

--- src/org/malibu/msu/bizapedia/ws/BizapediaBatchCallback.java
+++
@@ -1,9 +0,0 @@
-package org.malibu.msu.bizapedia.ws;
-
-import https.www_bizapedia.Company;
-import https.www_bizapedia.Principal;
-
-public interface BizapediaBatchCallback {
-	public void handleResult(Company company) throws Exception;
-	public void handleResult(Principal principle) throws Exception;
-}
